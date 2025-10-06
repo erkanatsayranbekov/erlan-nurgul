528da543b7c07418132a52c5a37df1c3714fc161
Ерканат Сайранбеков <erkanatsajranbekov@Erkanats-MacBook-Air.local>
2025-10-06 22:03:24 +0500

    fix

--- app/components/CountDown.jsx
+++ app/components/CountDown.jsx
@@ -2,6 +2,18 @@
 
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import localFont from 'next/font/local';
+
+const wg2 = localFont({
+    src: [
+    {
+        path: '../../public/KZ_Andantino_script_kz.ttf',
+        weight: '400',
+        style: 'normal',
+    },
+    ],
+    variable: '--font-wg',
+})
 
 function CountDown() {
 
@@ -42,36 +54,36 @@ function CountDown() {
     
     
     return (
-        <motion.div whileInView={{ opacity: 1, x: 0, }} initial={{ opacity: 0, x: -200 }} exit={{ opacity: 0 }} transition={{ duration: 1 }} className="grid grid-cols-5 sm:text-lg font-['wg'] bg-[rgba(0,0,0,0.3)] p-3 rounded-lg  bottom-40 text-white">
-            <div className="col-span-1 text-center text-2xl">
+        <motion.div whileInView={{ opacity: 1, x: 0, }} initial={{ opacity: 0, x: -200 }} exit={{ opacity: 0 }} transition={{ duration: 1 }} className="grid grid-cols-5 gap-x-4 font-extrabold sm:text-lg font-['wg'] p-3 rounded-lg  bottom-40 text-gray-800">
+            <div className={`${wg2.className} col-span-1 text-center text-4xl`}>
                 {weeks}
             </div>
-            <div className="col-span-1 text-center text-2xl">
+            <div className={`${wg2.className} col-span-1 text-center text-4xl`}>
                 {days}
             </div>
-            <div className="col-span-1 text-center text-2xl">
+            <div className={`${wg2.className} col-span-1 text-center text-4xl`}>
                 {hours}
             </div>
-            <div className="col-span-1 text-center text-2xl">
+            <div className={`${wg2.className} col-span-1 text-center text-4xl`}>
                 {minutes}
             </div>
-            <div className="col-span-1 text-center text-2xl">
+            <div className={`${wg2.className} col-span-1 text-center text-4xl`}>
                 {seconds}
             </div>
-            <div className="col-span-1 text-center">
-                {weeks === 1 ? 'Апта' : 'Апта'}
+            <div className={`${wg2.className} text-3xl col-span-1 text-center`}>
+                {weeks === 1 ? 'aпта' : 'aпта'}
             </div>
-            <div className="col-span-1 text-center">
-                {days === 1 ? 'Күн' : 'Күн'}
+            <div className={`${wg2.className} text-3xl col-span-1 text-center`}>
+                {days === 1 ? 'күн' : 'күн'}
             </div>
-            <div className="col-span-1 text-center">
-                {hours === 1 ? 'Сағат' : 'Сағат'}
+            <div className={`${wg2.className} text-3xl col-span-1 text-center`}>
+                {hours === 1 ? 'сағат' : 'сағат'}
             </div>
-            <div className="col-span-1 text-center">
-                {minutes === 1 ? 'Минута' : 'Минут'}
+            <div className={`${wg2.className} text-3xl col-span-1 text-center`}>
+                {minutes === 1 ? 'минута' : 'минут'}
             </div>
-            <div className="col-span-1 text-center">
-                {seconds === 1 ? 'Секунда' : 'Секунд'}
+            <div className={`${wg2.className} text-3xl col-span-1 text-center`}>
+                {seconds === 1 ? 'секунда' : 'секунд'}
             </div>
         </motion.div>
     );
--- app/page.js
+++ app/page.js
@@ -19,6 +19,18 @@ const wg = localFont({
   variable: '--font-wg',
 })
 
+
+const wg2 = localFont({
+  src: [
+    {
+      path: '../public/KZ_Andantino_script_kz.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-wg',
+})
+
 const CountDown = dynamic(() => import('./components/CountDown'), {
   ssr: false
 })
@@ -70,7 +82,7 @@ export default function Home() {
   return (
     <div className='font-[wg] text-center overflow-hidden'>
       <audio src="./music.mp3" autoPlay loop id='audio'></audio>
-    <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }}   className=' absolute top-16 right-[5%] z-10 ml-8'>
+    <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }}   className=' absolute top-[450px] left-[5%] z-10 ml-8'>
       <div className='flex space-x-2 justify-center items-center '>
         <img src="/play.png" alt="play" className={` h-[65px] w-[65px] mix-blend-luminosity absolute left-[10%] bg-white/70 hover:bg-white/90 rounded-full  ${play && 'animate-[spin_30s_linear_infinite]'}`}/>
         { play ? 
@@ -92,22 +104,28 @@ export default function Home() {
         <Image src="/flower-3.png" alt="flower-3" width={400} height={400} className=' absolute top-[300px] -right-64  z-20 ' />
         <Image src="/flower-4.png" alt="flower-4" width={400} height={400} className=' absolute top-[400px] left-24  z-10 animate-rotateWiggle' />
         <Image src="/flower-5.png" alt="flower-5" width={400} height={400} className=' absolute top-[500px] -left-52  z-0' />
-        <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -200}} transition={{ duration: 1 }} className=" absolute top-60 left-[10%] w-[250px] h-[180px] text-grey-100 p-3 rounded-lg text-center flex flex-col justify-center items-center text-gray-800">
-          <p className={`${wg.className} text-6xl text-start w-full`}>Ерлан</p>
-          <p className={`${wg.className} text-6xl`}>&</p>
-          <p className={`${wg.className} text-6xl text-end w-full`}>Нyргyль</p>
+        <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -200}} transition={{ duration: 1 }} className=" absolute top-60 left-[10%] w-[250px] h-[180px] text-grey-100 p-3 rounded-lg text-center flex flex-col justify-center items-center text-gray-600">
+          <i><p className={`${wg2.className} text-[64px]  text-start w-full`}>Ерлан</p>
+          <p className={`${wg.className} text-6xl `}>&</p>
+          <p className={`${wg2.className} text-[64px]  text-end w-full`}>Нұргүл</p></i> 
         </motion.div>
         <br />
         <br />
 
       </main>
+
+
+      <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }}  transition={{ duration: 1 }} className='w-[80%] font-bold mx-auto text-center my-[20px] relative'>
+      <p className={`${wg.className} font-bold text-8xl`}>22.11.2025</p>
+      <p className={`${wg.className} font-bold text-6xl`}> 17:00 | Сенбі</p>
+      </motion.div>
       
       <div className=' w-full flex flex-col justify-center items-center '>
         <CountDown />
         <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className=' text-center z-20 p-3 rounded-lg text-2xl'>
-          <h1 className='text-4xl'>Құрметті</h1>
-          <i><p className=' text-lg font-light'>
-            Құрметті ағайын-туыс, бауырлар, нағашы-жиен, бөлелер, құда-жегжат, дос-жарандар, көршілер мен әріптестер! <br />
+          <i><h1 className='text-4xl'>Құрметті</h1>
+          <p className=' text-lg font-light'>
+            ағайын-туыс, бауырлар, нағашы-жиен, бөлелер, құда-жекжат, дос-жарандар, көршілер мен әріптестер! <br />
             Сіз(дер)ді балаларымыз</p>
           </i>
         </motion.div>
@@ -121,7 +139,7 @@ export default function Home() {
 
       <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} transition={{ duration: 1 }} className='w-[80%] font-bold mx-auto text-center relative mb-[170px] border-[#96825f] border-[1px] '>
         <i>
-          <p className={`text-xl mt-6 m-0 absolute top-0 right-20`}>Eрлан мен Нургуль</p>
+          <p className={`text-3xl mt-6 m-0 absolute top-0 right-10`}>Eрлан мен Нұргүл</p>
           <p className=' absolute top-16 text-lg font-light'>Отау құру тойына арналған салтанатты ақ дастарханымыздың қадірлі қонағы болуға шақырамыз!
           </p>
         </i>
@@ -131,11 +149,8 @@ export default function Home() {
       <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }}  transition={{ duration: 1 }} className='w-[80%] font-bold mx-auto text-center relative border-[#96825f] border-[1px] '>
       </motion.div>
 
-      <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }}  transition={{ duration: 1 }} className='w-[80%] font-bold mx-auto text-center my-[20px] relative'>
-      <p className=' font-bold text-xl'>22.11.2025 | 17:00 | Сейсенбі</p>
-      </motion.div>
 
-      <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className=' flex flex-col w-[80%] mx-auto p-4 rounded-lg relative overflow-hidden right-0'>
+      <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className=' italic font-extrabold flex flex-col w-[80%] mx-auto p-4 rounded-lg relative overflow-hidden right-0'>
         {confetti &&
           <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className=' absolute top-0'>
             <Confetti width={window.innerWidth} height={window.innerHeight} />
@@ -146,28 +161,28 @@ export default function Home() {
         <div className=' flex flex-col gap-6 justify-between'>
           <div className='flex items-center'>
             <input id='default-radio-1' type='radio' value='ИӘ, КЕЛЕМІН' name='default-radio' className='w-4 h-4 outline-none text-blue-600 bg-gray-100 border-gray-300' onChange={(e) => setStatus(e.target.value)} />
-            <label for='default-radio-1' className='ms-2 text-xs font-medium  '>ИӘ, КЕЛЕМІН</label>
+            <label for='default-radio-1' className='ms-2 text-xs font-extrabold '>ИӘ, КЕЛЕМІН</label>
           </div>
           <div className='flex items-center'>
             <input id='default-radio-1' type='radio' value='ИӘ, КЕЛЕМІН' name='default-radio' className='w-4 h-4 outline-none text-blue-600 bg-gray-100 border-gray-300' onChange={(e) => setStatus(e.target.value)} />
-            <label for='default-radio-1' className='ms-2 text-xs font-medium  '>ИӘ, ЖОЛДАСЫММЕН КЕЛЕМІН</label>
+            <label for='default-radio-1' className='ms-2 text-xs font-extrabold '>ИӘ, ЖОЛДАСЫММЕН КЕЛЕМІН</label>
           </div>
           <div className='flex items-center'>
             <input id='default-radio-2' type='radio' value='ӨКІНІШКЕ ОРАЙ КЕЛЕ АЛМАЙМЫН' name='default-radio' className='w-4 h-4 outline-none text-blue-600 bg-gray-100 border-gray-300' onChange={(e) => setStatus(e.target.value)} />
-            <label for='default-radio-2' className='ms-2 text-xs font-medium  '>ӨКІНІШКЕ ОРАЙ КЕЛЕ АЛМАЙМЫН</label>
+            <label for='default-radio-2' className='ms-2 text-xs font-extrabold '>ӨКІНІШКЕ ОРАЙ КЕЛЕ АЛМАЙМЫН</label>
           </div>
         </div>
-        <button type='submit' className=' text-white bg-[#96825f] hover:bg-[#79633d] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2 dark:bg-[#96825f] dark:hover:bg-[#96825f] focus:outline-none dark:focus:ring-[#96825f]' onClick={Vote}>Растау</button>
+        <button type='submit' className=' mt-8 text-white bg-[#96825f] hover:bg-[#79633d] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 me-2 my-2 dark:bg-[#96825f] dark:hover:bg-[#96825f] focus:outline-none dark:focus:ring-[#96825f] text-xl' onClick={Vote}>Растау</button>
       </motion.div>
 
 
-      <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className=' flex flex-col w-[80%] mx-auto mb-[100px] ] p-4 rounded-lg relative overflow-hidden right-0 justify-center items-center text-lg '>
+      <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className=' flex flex-col w-[80%] mx-auto mb-[100px] ] p-4 rounded-lg relative overflow-hidden right-0 justify-center items-center text-lg gap-3 '>
         <img src="/favicon.png" alt="2gis" className=' h-[100px] w-[100px] mt-12' onClick={(e) => {
           window.location.href = 'https://go.2gis.com/jQ3S9'
         }} />
-        <p className=' text-2xl font-extrabold'>Астана қаласы</p>
-        <p className=' text-lg font-extrabold'>Шығыс жұлдызы мейрамханасы</p>
-        <p className=' text-lg font-extrabold'>Темирбек Жургенов, 18/2</p>
+        <p className=' text-4xl font-extrabold'>Астана қаласы</p>
+        <p className=' text-2xl font-extrabold'>"Шығыс жұлдызы" мейрамханасы</p>
+        <p className=' text-2xl font-extrabold'>Темирбек Жургенов, 18/2</p>
       </motion.div>
 
       <YMaps query={{ lang: 'ru_RU', apikey: '536168cc-8dbb-4923-a06f-9a6bd5a9cf15' }}>
